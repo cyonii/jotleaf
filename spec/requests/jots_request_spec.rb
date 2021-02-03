865cb61535386b2f4f10aaa3064c4425ffa94e82
CY Kalu <cy23@tutanota.com>
2021-02-03 11:59:30 +0100

    Add more test coverage

--- spec/requests/jots_request_spec.rb
+++ spec/requests/jots_request_spec.rb
@@ -21,37 +21,43 @@ RSpec.describe 'Jots', type: :request do
 
   it 'gets new jot article page' do
     get new_jot_path
+    expect(response).to render_template(:new)
     expect(response.body).to include('Jot it down')
   end
 
   it 'creates a new jot article' do
     post jots_path, params: jot_params
-    follow_redirect!
+    expect(response).to redirect_to(assigns(:jot))
 
+    follow_redirect!
     expect(response.body).to include('Jot created successfully')
+    expect(response).to render_template(:show)
   end
 
   it 'gets jot show page' do
     get jot_path(jot)
+    expect(response).to render_template(:show)
     expect(response.body).to include(jot_params[:jot][:title])
   end
 
   it 'gets edit jot article page' do
     get edit_jot_path(jot)
+    expect(response).to render_template(:edit)
     expect(response.body).to include('Edit jot')
   end
 
   it 'updates jot article' do
     patch jot_path(jot), params: { jot: { title: 'Edited' } }
-    follow_redirect!
+    expect(response).to redirect_to(assigns(:jot))
 
+    follow_redirect!
     expect(response.body).to include('Jot successfully updated')
   end
 
   it 'deletes jot article' do
     delete jot_path(jot)
-    follow_redirect!
 
+    follow_redirect!
     expect(response.body).to include('Jot deleted successfully')
   end
 end
